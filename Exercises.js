046b1782e97afc21384e462943ed499b06a564b7
Arpi Avetisyan <arpi.avetisyan.1998@gmail.com>
2020-10-13 17:30:28 +0400

    Added some new exercises from "You Don't Know JS".

--- Exercises.js
+++ Exercises.js
@@ -1,15 +1,36 @@
-const accessory_price = 9.99;
+// const accessory_price = 9.99;
 
-var bank_balance = 302.12;
-var amount = 99.9;
+// var bank_balance = 302.12;
+// var amount = 99.9;
 
-amount = amount * 2;
+// amount = amount * 2;
 
-if (amount < bank_balance) {
-    console.log("I will take the accessory.")
-    amount = amount + accessory_price;
-} else {
-    console.log("No, thank you.")
+// if (amount < bank_balance) {
+//     console.log("I will take the accessory.")
+//     amount = amount + accessory_price;
+// } else {
+//     console.log("No, thank you.")
+// }
+
+// console.log(amount.toFixed(2))
+
+// var i = 0;
+
+// while (true) {
+//     if ((i <= 9) === false) {
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// for (var i = 0; i <= 9; i++) {
+//     console.log(i)
+// }
+
+function printAmount() {
+    console.log(amount.toFixed(2));
 }
-
-console.log(amount.toFixed(2))
+var amount = 12.97;
+amount = amount * 2;
+printAmount();
