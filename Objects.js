6c556f00d68653b47b377f1b0b5212597a0e399f
Arpi Avetisyan <arpi.avetisyan.1998@gmail.com>
2020-10-18 17:12:51 +0400

    Basic hints on Objects-JS.

---
+++ Objects.js
@@ -0,0 +1,23 @@
+//var obj = {
+a: "Hello world",
+    b: 42,
+    c: true,
+};
+
+console.log(obj.a)
+console.log(obj.b)
+console.log(obj.c)
+
+//obj["a"];
+//obj["b"];
+//obj["c"]
+
+var obj = {
+    a: "Hello world",
+    b: 42,
+}
+
+var b = "a";
+
+console.log(obj[b])
+    // console.log(obj["b"])
