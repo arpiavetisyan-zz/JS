5e8fd0ec730236e6f87b27d50965f147bce2f6c7
Arpi Avetisyan <arpi.avetisyan.1998@gmail.com>
2020-10-28 15:31:14 +0400

    Solved problem on topic "Common character count".

---
+++ commonCharacterCount.js
@@ -0,0 +1,13 @@
+function commonCharacterCount(s1, s2) {
+    var count = 0;
+    while (s1.length && s2.length) {
+        if (s2.includes(s1.charAt(0))) {
+            count++;
+            s2 = s2.replace(s1.charAt(0), "");
+            s1 = s1.slice(1);
+        } else {
+            s1 = s1.slice(1);
+        }
+    }
+    return count;
+}
