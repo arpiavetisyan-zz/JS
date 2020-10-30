c5e7a8d28da07746fb1cb28d23ea90e938287eb6
Arpi Avetisyan <arpi.avetisyan.1998@gmail.com>
2020-10-30 18:19:29 +0400

    Given a ticket number n, determine if it's lucky or not.

---
+++ isLucky.js
@@ -0,0 +1,18 @@
+function isLucky(n) {
+    n = n.toString();
+    var half = n.length / 2;
+    var first = 0;
+    var second = 0;
+    for (var i = 0; i < half; i++) {
+        first += +n[i];
+    }
+    for (var j = half; j < n.length; j++) {
+        second += +n[j];
+    }
+
+    if (first != second) {
+        return false;
+    } else {
+        return true;
+    }
+}
